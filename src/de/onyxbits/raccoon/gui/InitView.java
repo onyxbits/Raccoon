7fa205404b856f226eb6f406a74d4c0dc40e93a8
patrick <github@onyxbits.de>
2014-06-08 12:33:05 +0200

    Finalized Loginscreen

--- src/de/onyxbits/raccoon/gui/InitView.java
+++ src/de/onyxbits/raccoon/gui/InitView.java
@@ -1,6 +1,5 @@
 package de.onyxbits.raccoon.gui;
 
-import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -8,11 +7,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import de.onyxbits.raccoon.BrowseUtil;
@@ -25,11 +23,11 @@ public class InitView extends JPanel implements ActionListener {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private JTextField password;
+	private JPasswordField password;
 	private JTextField userId;
 	private JTextField androidId;
 	private JLabel status;
-	private JButton create;
+	private JButton login;
 	private JButton help;
 
 	private Archive archive;
@@ -39,28 +37,51 @@ public class InitView extends JPanel implements ActionListener {
 	private InitView(MainActivity mainActivity, Archive archive) {
 		this.mainActivity = mainActivity;
 		this.archive = archive;
-		password = new JTextField();
+		password = new JPasswordField("", 20);
 		userId = new JTextField("", 20);
-		androidId = new JTextField();
+		androidId = new JTextField("", 20);
 		status = new JLabel(" ");
-		create = new JButton("Create");
+		login = new JButton("Login");
 		help = new JButton("Help");
 		JLabel instr = new JLabel(
-				"<html>This archive needs to be linked to a Google account and a device. The Google account is required, the Android ID is optional (a new one will automatically be generated if none is given). Press 'Help' for details.</html>");
-		instr.setAlignmentX(Component.CENTER_ALIGNMENT);
-		add(instr);
-		add(Box.createVerticalGlue());
+				"<html>This archive needs to be linked to a Google account and a device. The Google account is required, the device ID will automatically be generated if none is given. Press 'Help' for details.</html>");
 
-		JPanel outer = new JPanel();
-
-		outer.add(createCredentials());
-		add(outer);
 		JPanel container = new JPanel();
-		container.add(create);
 		container.add(help);
-		add(container);
-		add(status);
-		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		container.add(login);
+
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		gbc.insets = new Insets(2, 2, 2, 2);
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2;
+		gbc.gridheight = 1;
+		gbc.weightx = 2;
+		gbc.weighty = 2;
+		gbc.anchor = GridBagConstraints.CENTER;
+		add(instr, gbc);
+
+		gbc.gridy = 1;
+		gbc.gridwidth = 1;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.fill = GridBagConstraints.BOTH;
+		JPanel outer = new JPanel(); // GridBaglayout...
+		outer.add(createCredentials());
+		add(outer, gbc);
+
+		outer = new JPanel();
+		outer.add(status);
+		gbc.gridy = 2;
+		add(outer, gbc);
+
+		gbc.gridy = 3;
+		add(container, gbc);
+
+		setBorder(BorderFactory.createEmptyBorder(50, 100, 50, 100));
 	}
 
 	private JPanel createCredentials() {
@@ -72,41 +93,46 @@ public class InitView extends JPanel implements ActionListener {
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		gbc.weightx = 0;
 		ret.add(new JLabel("Username:"), gbc);
 		gbc.gridx = 1;
 		gbc.gridy = 0;
+		gbc.weightx = 1;
 		ret.add(userId, gbc);
 		gbc.gridx = 0;
 		gbc.gridy = 1;
+		gbc.weightx = 0;
 		ret.add(new JLabel("Password:"), gbc);
 		gbc.gridx = 1;
 		gbc.gridy = 1;
+		gbc.weightx = 1;
 		ret.add(password, gbc);
 		gbc.gridx = 0;
 		gbc.gridy = 2;
 		gbc.insets = new Insets(14, 2, 2, 2);
+		gbc.weightx = 0;
 		ret.add(new JLabel("Android ID:"), gbc);
 		gbc.gridx = 1;
 		gbc.gridy = 2;
+		gbc.weightx = 1;
 		ret.add(androidId, gbc);
 		return ret;
 	}
 
 	public static InitView create(MainActivity mainActivity, Archive archive) {
 		InitView ret = new InitView(mainActivity, archive);
-		ret.setLayout(new BoxLayout(ret, BoxLayout.Y_AXIS));
-		ret.create.addActionListener(ret);
+		ret.login.addActionListener(ret);
 		ret.help.addActionListener(ret);
 		return ret;
 	}
 
 	public void actionPerformed(ActionEvent event) {
 		Object src = event.getSource();
-		if (src == create) {
+		if (src == login) {
 			archive.setPassword(password.getText());
 			archive.setUserId(userId.getText());
 			archive.setAndroidId(androidId.getText());
-			create.setEnabled(false);
+			login.setEnabled(false);
 			new InitWorker(archive, this).execute();
 		}
 		if (src == help) {
@@ -120,7 +146,7 @@ public class InitView extends JPanel implements ActionListener {
 
 	protected void doErrorMessage() {
 		status.setText("Bad credentials");
-		create.setEnabled(true);
+		login.setEnabled(true);
 	}
 
 	protected void doInProgress() {
