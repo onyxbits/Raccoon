9cd4bdad2ef0c91de5c13b4a502059c6b79413b6
patrick <github@onyxbits.de>
2014-08-06 11:58:04 +0200

    Preemptive version bump

--- src/main/java/de/onyxbits/raccoon/App.java
+++ src/main/java/de/onyxbits/raccoon/App.java
@@ -21,7 +21,7 @@ public class App {
 	/**
 	 * Version identifier.
 	 */
-	public static final String VERSIONSTRING = "3.1";
+	public static final String VERSIONSTRING = "3.2";
 
 	/**
 	 * Relative path for keeping extension jars in
